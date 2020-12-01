3076c02f54b6bc01ba4fb429c3a5c5efeb577e55
Jonathan Bogg <jonathan.bogg@gmail.com>
2020-12-02 00:11:13 +0100

    Init aws amplify

--- millap/src/App.tsx
+++ millap/src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <AmplifySignOut />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -23,4 +23,4 @@ function App() {
   );
 }
 
-export default App;
+export default withAuthenticator(App);
