db78cd7ff16dc91f3131c22bf1d611568b6bb080
Jonathan Bogg <jonathan.bogg@gmail.com>
2020-12-02 23:12:30 +0100

    Rehaul of configs, add WIP page

---
+++ millap/src/Root.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './assets/fontface/didot/didot.css';
+// import { useSpring, animated } from 'react-spring';
+
+const Root = (): React.ReactElement => (
+  <div
+    style={{
+      fontFamily: 'Didot',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginTop: '-6.5rem',
+      letterSpacing: '0.02rem',
+      fontWeight: 'bold',
+      color: 'rgb(14, 13, 13)'
+    }}
+  >
+    <p style={{ margin: 0, fontSize: '3.2rem' }}>Milla Flyger</p>
+    <p>coming soon</p>
+  </div>
+);
+
+export default Root;
