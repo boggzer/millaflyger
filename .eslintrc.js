74b02375f34e5727396b2e39ab41b238c6e6d1ac
Jonathan Bogg <jonathan.bogg@gmail.com>
2020-12-04 01:35:56 +0100

    Update eslint configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,42 +1,43 @@
 module.exports = {
-	root: true,
-	env: {
-		browser: true,
-		es6: true,
-		node: true
-	},
-	files: ['**/*.ts?(x)'],
-	parser: '@typescript-eslint/parser',
-	extends: [
-		'eslint:recommended',
-		'plugin:react/recommended',
-		'plugin:@typescript-eslint/recommended',
-		'plugin:react-hooks/recommended',
-		'prettier',
-		'prettier/react'
-	],
-	parserOptions: {
-		ecmaFeatures: {
-			'jsx': true
-		},
-		ecmaVersion: 2018,
-		sourceType: 'module'
-	},
-	plugins: [
-		'react',
-		'eslint-plugin-react',
-		'@typescript-eslint'
-	],
-	rules: {
-		quotes: ['error', 'single'],
-		indent: ['error', 2], 
-		'no-trailing-spaces': 'error',
-		'no-console': 'error',
-		'no-unused-vars': 'warn' 
-	},
-	settings: {
-		react: {
-			version: 'latest'
-		}
-	}
-};
+  root: true,
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'react-hooks',
+    'eslint-plugin-react',
+    '@typescript-eslint',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+    'prettier/react',
+  ],
+
+  rules: {
+    quotes: ['error', 'single'],
+    indent: ['error', 2],
+    'no-trailing-spaces': 'error',
+    'no-console': 'error',
+    'no-unused-vars': 'warn',
+  },
+  settings: {
+    react: {
+      version: 'latest',
+    },
+  },
+}
