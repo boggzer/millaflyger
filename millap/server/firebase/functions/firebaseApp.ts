bb57c18e0ef5b4a3be6e2721cba5ec7211552add
Jonathan G. Bogg <55447943+boggzer@users.noreply.github.com>
2021-01-08 01:19:09 +0100

    Remove server

--- millap/server/firebase/functions/firebaseApp.ts
+++
@@ -1,7 +0,0 @@
-import { firebase, config } from './config/firebase-config';
-
-const firebaseApp = firebase.initializeApp(config);
-
-const firebaseDb = firebaseApp.firestore();
-
-export { firebaseDb, firebaseApp };
--- millap/server/routers/imageRouter.ts
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import { getImageById, getImagesByTitle } from '../controllers/imageController';
-
-const router = express.Router();
-
-router.get('/image', getImageById);
-router.get('/', getImagesByTitle);
