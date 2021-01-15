74eef2e71c2055f3cfe9061014552130a1ce3182
Jonathan G. Bogg <55447943+boggzer@users.noreply.github.com>
2021-01-15 02:51:24 +0100

    Update loads of scss

--- millap/client/src/hooks/useRefChange.tsx
+++ millap/client/src/hooks/useRefChange.tsx
@@ -2,7 +2,11 @@
 import React, { useCallback } from 'react';
 
 const useRefChange = (
-  setRef: React.Dispatch<React.SetStateAction<HTMLElement | undefined>>,
+  setRef: React.Dispatch<
+    React.SetStateAction<
+      HTMLElement | Element | HTMLImageElement | HTMLDivElement | undefined
+    >
+  >,
 ): ((node: any) => void) => {
   const onRefChange = useCallback((node) => {
     setRef(node);
