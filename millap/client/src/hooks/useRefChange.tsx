7f7878db219f8caa4daa52648f347634ff60eb90
Jonathan G. Bogg <55447943+boggzer@users.noreply.github.com>
2021-01-23 18:10:35 +0100

    Remove unused code

--- millap/client/src/hooks/useRefChange.tsx
+++ millap/client/src/hooks/useRefChange.tsx
@@ -1,19 +1,10 @@
 import React, { useCallback } from 'react';
 
-type RefNode =
-  | HTMLElement
-  | HTMLImageElement
-  | HTMLDivElement
-  | undefined
-  | null;
+type RefNode = HTMLElement | HTMLImageElement | HTMLDivElement | undefined;
 
 type HookReturn = (refNode: RefNode) => void;
 
-type HookProps = React.Dispatch<
-  React.SetStateAction<
-    HTMLElement | Element | HTMLImageElement | HTMLDivElement | undefined
-  >
->;
+type HookProps = React.Dispatch<React.SetStateAction<RefNode>>;
 
 const useRefChange = (setRef: HookProps): HookReturn => {
   const onRefChange = useCallback((refNode) => {
