5fc26abd24ad03ee4dbc64a38bd77d49d4c58be9
Jonathan G. Bogg <55447943+boggzer@users.noreply.github.com>
2021-01-27 13:52:32 +0100

    Remove comments

--- millap/client/src/components/Layout.tsx
+++ millap/client/src/components/Layout.tsx
@@ -1,26 +1,18 @@
 import React, { Suspense, lazy, useContext } from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  RouteProps,
-  matchPath,
-  RouteChildrenProps,
-  RouteComponentProps,
-} from 'react-router-dom';
-// import { ProjectsContext } from '../contexts/projectsContext';
-import Navigation from './presentational/Navigation';
-// const Overview = lazy(() => import('./pages/portfolio/Overview'));
-import ErrorBoundary from '../utils/ErrorBoundary';
-// import { full, singles } from './data';
-import Container from './presentational/Container';
+import { BrowserRouter as Router, Route, matchPath } from 'react-router-dom';
 import { CSSTransition } from 'react-transition-group';
+import slugify from 'slugify';
+
 const NotFound = lazy(() => import('./pages/NotFound'));
 const Overview = lazy(() => import('./pages/portfolio/Overview'));
-import About from './pages/About';
-import Start from './pages/Start';
 const Project = lazy(() => import('./presentational/Project'));
-import slugify from 'slugify';
-import { ProjectDataType, ProjectsType } from '../utils/global';
+import About from './pages/About';
+import Container from './presentational/Container';
+import ErrorBoundary from '../utils/ErrorBoundary';
+import Navigation from './presentational/Navigation';
+import Start from './pages/Start';
+
+import { ProjectDataType } from '../utils/global';
 import { ProjectsContext } from '../contexts/projectsContext';
 
 type RouteType = {
@@ -38,45 +30,6 @@ const Layout = (): React.ReactElement => {
     () => ({ full: projects?.full || [], singles: projects?.singles || [] }),
     [projects],
   );
-  /*
-  const fetched = data.reduce((acc: any, { images }, ind: number) => {
-    const d = data;
-    const h = images.map((v: any, ing: any) => {
-      const newD: any = d[ind].images[ing]['sizes'];
-      data[ind].images[ing]['sizes'] = [];
-      return keys(images[ing]?.source[0]).reduce(
-        (acct: any, v: any, i: any) => {
-          d[ind].images[ing]['sizes'] = [];
-          const size = { [v]: getImageSize(d[ind].images[ing].source[0]?.[v] }
-          getImageSize(d[ind].images[ing].source[0]?.[v]);
-          const dd = getImageSize(d[ind].images[ing].source[0]?.[v] || '');
-          newD.push({ [v]: dd });
-          acct = data;
-          return { ...acct, ...data };
-        },
-        {},
-      );
-    });
-    return {
-      ...acc,
-      h,
-    };
-  }, {});
-*/
-  /*
-    data.map(({ images, ...rest }, index: number) => {
-      images.map((v: any, i: number, arr: any[]) => {
-        keys(data[index]?.images[i]?.source[0]).map((k) => {
-          // data[index].images[i]['sizes'][0][k] = {
-          //   ...getImageSize(data[index]?.images[i]?.source[0]?.[k]),
-          // };
-          console.log(v);
-        });
-        console.log();
-      });
-    });
-    */
-  // console.log(fetched, 'hello');
 
   const setPageTitle = (pageTitle: string) => {
     document.title = pageTitle;
