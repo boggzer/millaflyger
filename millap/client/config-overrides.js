7a6971545a1f7012e29025507593b0653e5bd934
Jonathan G. Bogg <55447943+boggzer@users.noreply.github.com>
2021-01-28 21:08:09 +0100

    Update config-overrides.js

--- millap/client/config-overrides.js
+++ millap/client/config-overrides.js
@@ -4,10 +4,11 @@ const { override } = require('customize-cra');
 const cspHtmlWebpackPlugin = require('csp-html-webpack-plugin');
 
 const cspConfigPolicy = {
+  'default-src': "'self'",
   'base-uri': "'self'",
   'object-src': "'none'",
   'script-src': ["'self'"],
-  'style-src': ["'self'"],
+  'style-src': ['unsafe-inline'],
   'img-src': ["'self'", 'https://firebasestorage.googleapis.com'],
   'connect-src': [
     "'self'",
