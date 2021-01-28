c608d853b843bdc8d33bb8bafc58a99906e1ec09
Jonathan G. Bogg <55447943+boggzer@users.noreply.github.com>
2021-01-28 20:48:29 +0100

    Add temporary CSP

--- millap/client/config-overrides.js
+++ millap/client/config-overrides.js
@@ -4,11 +4,17 @@ const { override } = require('customize-cra');
 const cspHtmlWebpackPlugin = require('csp-html-webpack-plugin');
 
 const cspConfigPolicy = {
-  'default-src': "'none'",
   'base-uri': "'self'",
   'object-src': "'none'",
   'script-src': ["'self'"],
   'style-src': ["'self'"],
+  'img-src': ["'self'", 'https://firebasestorage.googleapis.com'],
+  'connect-src': [
+    "'self'",
+    'https://cognito-identity.eu-central-1.amazonaws.com',
+    'https://of655rwde0.execute-api.eu-central-1.amazonaws.com',
+  ],
+  'manifest-src': ["'self'"],
 };
 
 function addCspHtmlWebpackPlugin(config) {
