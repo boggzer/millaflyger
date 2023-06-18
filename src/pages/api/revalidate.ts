7c3fea71cc37a12a18f9b0d1a4622ec3c7184457
Jonathan <speljavel@gmail.com>
2023-06-18 17:47:02 +0200

    more logs

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -1,13 +1,13 @@
 import { isValidSignature, SIGNATURE_HEADER_NAME } from '@sanity/webhook'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-const secret = process.env.SANITY_WEBHOOK_SECRET;
+const secret = process.env.SANITY_REVALIDATE_TOKEN;
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Record<string, unknown>>) {
     const signature = req.headers[SIGNATURE_HEADER_NAME]
     const body = await readBody(req) // Read the body into a string
-
-    const message = [`Secret: ${secret}`, `Signature: ${signature}`, `Body: ${body}`]
+    
+    const message = [`Secret: ${secret}`, `Signature: ${signature}`, `Body: ${body}`, Object.keys(req.headers).join('/')]
     if (!isValidSignature(body, signature as any, secret)) {
         //res.status(401).json({ success: false, message: `Invalid signature - ${secret}, ${body}, ${signature}`, })
         //return
