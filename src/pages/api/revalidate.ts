fea2362e3a3e5e0875089a0d9c70d266cf6c18a9
Jonathan <speljavel@gmail.com>
2023-06-18 16:28:14 +0200

    now?

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -1,4 +1,4 @@
-import { parseBody } from 'next-sanity/webhook'
+import { isValidSignature, SIGNATURE_HEADER_NAME } from '@sanity/webhook'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
@@ -8,21 +8,15 @@ type Data = {
 const secret = process.env.SANITY_WEBHOOK_SECRET || '123TEST' // temporary during dev
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-    if (req.method !== 'POST') {
-        console.error('Must be a POST request')
-        return res.status(405).json({ message: 'Must be a POST request' })
-    }
-
-    const { isValidSignature, body } = await parseBody(req, secret);
-
-    if (!isValidSignature) {
-        const message = 'Invalid signature';
-        console.warn(message);
-        return res.status(401).json({ message });
+    const signature = req.headers[SIGNATURE_HEADER_NAME]
+    const body = await readBody(req) // Read the body into a string
+    if (!isValidSignature(body, signature, secret)) {
+        res.status(401).json({ success: false, message: 'Invalid signature' })
+        return
     }
 
     try {
-        const { type, slug } = body;
+        const { body: { type, slug } } = req;
 
         await res.revalidate(`https://nextjs-ssr-test.d3v2rqv1ub3q0i.amplifyapp.com/projects/${slug}`)
         await res.revalidate(`https://nextjs-ssr-test.d3v2rqv1ub3q0i.amplifyapp.com/projects`)
@@ -32,4 +26,19 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
     } catch (err) {
         return res.status(500).send({ message: 'Error revalidating' })
     }
+}
+
+// Next.js will by default parse the body, which can lead to invalid signatures
+export const config = {
+    api: {
+        bodyParser: false,
+    },
+}
+
+async function readBody(readable) {
+    const chunks = []
+    for await (const chunk of readable) {
+        chunks.push(typeof chunk === 'string' ? Buffer.from(chunk) : chunk)
+    }
+    return Buffer.concat(chunks).toString('utf8')
 }
