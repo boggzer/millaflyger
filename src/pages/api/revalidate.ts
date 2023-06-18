393d41112cc2f8c6399f556df12156d4e8895522
Jonathan <speljavel@gmail.com>
2023-06-18 18:24:20 +0200

    fix req read

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -1,21 +1,19 @@
 import { isValidSignature, SIGNATURE_HEADER_NAME } from '@sanity/webhook'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-const secret = process.env.SANITY_REVALIDATE_TOKEN;
-
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Record<string, unknown>>) {
     const signature = req.headers[SIGNATURE_HEADER_NAME]
     const body = await readBody(req) // Read the body into a string
     
-    const message = [`Secret: ${secret}`, `Signature: ${signature}`, `Body: ${body}`, Object.keys(req.headers).join('/')]
-    if (!isValidSignature(body, signature as any, secret)) {
+    const message = [`Secret: ${process.env.SANITY_REVALIDATE_TOKEN}`, `Signature: ${signature}`, `Body: ${body}`, Object.keys(req.headers).join('/')]
+    if (!isValidSignature(body, signature as any, process.env.SANITY_REVALIDATE_TOKEN)) {
         //res.status(401).json({ success: false, message: `Invalid signature - ${secret}, ${body}, ${signature}`, })
         //return
         message.push('Invalid signature.')
     }
 
     try {
-        message.push(`Slug: ${JSON.parse(req.body)?.slug}`)
+        message.push(`Slug: ${JSON.parse(body)?.slug}`)
         //await res.revalidate(`https://nextjs-ssr-test.d3v2rqv1ub3q0i.amplifyapp.com/projects/${slug}`)
         await res.revalidate(`/projects`)
 
