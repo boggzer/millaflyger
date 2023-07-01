ee19c76cfe6cf66eb46448dcbaba1215e64a0f62
Jonathan <speljavel@gmail.com>
2023-07-01 19:46:00 +0200

    fix: import

--- src/components/ExitPreviewButton.tsx
+++ src/components/ExitPreviewButton.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
 import styles from '@styles/exitPreviewButton.module.scss';
-import Button from './Button';
+import { Button } from '@components';
 
 export default function ExitPreviewButton() {
   return (
